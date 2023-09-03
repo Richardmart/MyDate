161bf9f7bef899094969e511b29e59f9719a0ed8
Richard Martinez <richardmart09@hotmail.com>
2023-09-03 18:43:05 -0400

    Create constructors

--- src/MyDate.java
+++ src/MyDate.java
@@ -1,3 +1,18 @@
 public class MyDate {
-    
+    //Create datafields for MyDate class
+    int year;
+    int month;
+    int day;
+    //no-arg constructor
+    public MyDate(){
+
+    }
+    //constructor using elapsed time in milliseconds
+    public MyDate(long elapsedTime){
+
+    }
+    //constructor using specified year, month, and day inputted
+    public MyDate(int year, int month, int day){
+
+    }
 }

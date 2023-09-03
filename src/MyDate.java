d9480540f251b78950ed07b6a726f4d96e22ee91
Richard Martinez <richardmart09@hotmail.com>
2023-09-03 18:58:42 -0400

    Getters set

--- src/MyDate.java
+++ src/MyDate.java
@@ -1,6 +1,7 @@
 public class MyDate {
     //Create datafields for MyDate class
     int year;
+    //month is 0 based, Jan starts at 0. Dec is 11
     int month;
     int day;
     //no-arg constructor
@@ -14,5 +15,22 @@ public class MyDate {
     //constructor using specified year, month, and day inputted
     public MyDate(int year, int month, int day){
 
+    }
+
+    //get year
+    public int getYear(){
+        return year;
+    }
+    //get month
+    public int getMont(){
+        return month;
+    }
+    //get day
+    public int getDay(){
+        return day;
+    }
+    //set date using long in miliseconds
+    public void setDate(long elapsedTime){
+         
     }
 }
